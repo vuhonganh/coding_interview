81df551a00dc3f801a33ef0eb2532991df159f46
hvu <vuhonganh91@gmail.com>
2017-09-17 22:46:58 +0100

    accepted Median of Two Sorted Arrays

--- LeetCode/longest_substring_no_repeat.py
+++ LeetCode/longest_substring_no_repeat.py
@@ -14,3 +14,19 @@ class Solution(object):
             cur_queue.append(c)
             max_len = max(max_len, len(cur_queue))         
         return max_len
+
+
+    def lengthOfLongestSubstring_v2(self, s):
+        """
+        use of len can be slow, use a dict instead
+        """
+        char_to_idx = {}
+        max_len = 0
+        idx_start = 0  # start idx of the non-repeated substring
+        for idx, c in enumerate(s):
+            if c in char_to_idx and idx_start <= char_to_idx[c]:  # if found a duplication
+                idx_start = char_to_idx[c] + 1  # update substring but cutting the previous part
+            char_to_idx[c] = idx
+            max_len = max(max_len, idx - idx_start + 1)  # cur_length = idx - idx_start + 1
+        return max_len
+
