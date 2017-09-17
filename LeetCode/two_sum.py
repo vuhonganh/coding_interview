a846ed5c303370124e2ce356b533e3e6d53b453d
hvu <vuhonganh91@gmail.com>
2017-09-17 15:55:21 +0100

    add ver2 which is faster: only one pass

--- LeetCode/two_sum.py
+++ LeetCode/two_sum.py
@@ -4,8 +4,7 @@ class Solution:
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        """
-        
+        """        
         # first pass get all the val to idx
         val_to_idx = {nums[idx]:idx for idx in range(len(nums))}
         # second pass:
@@ -13,5 +12,20 @@ class Solution:
             rest = target - nums[cur_idx]
             if rest in val_to_idx and val_to_idx[rest] != cur_idx:
                 return [cur_idx, val_to_idx[rest]]
+        print('sth wrong')
+        return
+    def twoSum_v2(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        val_to_idx = {}
+        # only one pass:
+        for cur_idx in range(len(nums)):
+            rest = target - nums[cur_idx]
+            if rest in val_to_idx:
+                return [cur_idx, val_to_idx[rest]]
+            val_to_idx[nums[cur_idx]] = cur_idx
         print('sth wrong')
         return
